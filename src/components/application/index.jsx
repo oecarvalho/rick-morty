d3286af94bf012c96f51019eb8214f031725fc62
felipecarvalho <felipe.carvalho_2016@outlook.com>
2024-10-05 00:08:38 -0300

    finalizando api

--- src/components/application/index.jsx
+++ src/components/application/index.jsx
@@ -1,26 +1,69 @@
+import { useEffect, useState } from "react";
 import { CardCharacter } from "../CardCharacters";
-import { ContainerApp, ContentCharacters, HeaderApp } from "./style";
+import { ContainerApp, ContentCharacters, HeaderApp, Loader } from "./style";
+import IconLoader from '../../assets/loader.gif'
+import axios from "axios";
 
 export function Aplication(){
 
+    const [characters, setCharacters] = useState([]);
+    const [page, setPage] = useState(1);
+    const [countPages, setCountPages] = useState();
+    const [qntCharacters, setQntCharacters] = useState();
+    const [isLoader, setIsLoader] = useState(true);
+
+    useEffect(()=>{
+        axios.get(`https://rickandmortyapi.com/api/character?page=${page}`)
+        .then(response => {
+            const array = [...characters, ...response.data.results]
+            setCharacters(array)
+            setCountPages(response.data.info.pages)
+            setQntCharacters(response.data.info.count)
+            setIsLoader(false)
+        })
+    }, [page])
+
     return(
-        <ContainerApp>
+
+        <>
+        {
+            isLoader && (
+                <Loader>
+                    <img src={IconLoader} alt="" />
+                </Loader> 
+            )
+        }
+        <ContainerApp> 
+            
             <HeaderApp>
                 <h1>Rick and Morty</h1>
-                <span>Nº de Personagens: 896</span>
+                <span>Nº de Personagens: {qntCharacters}</span>
             </HeaderApp>
 
             <ContentCharacters>
                 <div>
-                    <CardCharacter
-                        image='https://rickandmortyapi.com/api/character/avatar/322.jpeg'
-                        name='Rick Sanchez'
-                        gender='Male'
-                        specie='Human'
-                    />
+                    {
+                      characters && characters.map(({image, name, species, gender}) => {
+                       return(
+                        <CardCharacter
+                            image={image}
+                            name={name}
+                            gender={species}
+                            specie={gender} 
+                        />
+                       )
+                      })
+                    }
+                    
                 </div>
-                <button>Carregar Mais</button>
+
+                {
+                    (!(page == countPages)) && <button onClick={()=> setPage(page + 1)}>Carregar Mais</button>
+                }
+                
             </ContentCharacters>
         </ContainerApp>
+        </>
+
     )
 }
--- src/components/application/style.js
+++ src/components/application/style.js
@@ -49,3 +49,16 @@ export const ContentCharacters = styled.div`
         }
     }
 `
+
+export const Loader = styled.div`
+    width: 100%;
+    height: 100vh;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 200;
+    background-color: #040011;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
